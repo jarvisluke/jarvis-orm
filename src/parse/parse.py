437aed3012444fe3724367d32c73b848661a65f4
LUKE JARVIS <jarvislukeldj@gmail.com>
2024-03-04 21:40:02 -0500

    removed typehinting

--- src/parse/parse.py
+++ src/parse/parse.py
@@ -9,14 +9,14 @@ class CommandParser:
     def __init__(self, cmds: Dict[str, Command]) -> None:
         self.parser = ArgumentParser()
         self.subp = self.parser.add_subparsers(dest="cmd")
-        self.cmds: Dict[str, Command]= {}
+        self.cmds: Dict[str, Command] = {}
         # Initializes commands
         for name, cls in cmds.items():
             self.cmds[name] = cls(self.subp)
         
     def parse_args(self):
         args = self.parser.parse_args() 
-        cmd: Command = self.cmds[args.cmd]
+        cmd = self.cmds[args.cmd]
         cmd.run(args)
         
 
